3afdb5089ec14a6a5c0f2c7cecfea2d75a53dc2d
haopo <haopoboy@gmail.com>
2019-04-04 22:52:57 +0800

    Fix tests and importing wrong JSON lib

--- src/test/kotlin/com/github/haopoboy/docmd/controller/ApiV1ControllerUnitTests.kt
+++ src/test/kotlin/com/github/haopoboy/docmd/controller/ApiV1ControllerUnitTests.kt
@@ -1,14 +1,19 @@
 package com.github.haopoboy.docmd.controller
 
+import com.fasterxml.jackson.databind.ObjectMapper
 import com.github.haopoboy.docmd.entity.Person
 import com.github.haopoboy.docmd.service.RepositoryService
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.mockito.ArgumentMatchers
 import org.mockito.BDDMockito.given
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.boot.test.mock.mockito.MockBean
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.PageImpl
+import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.http.MediaType
 import org.springframework.test.context.junit4.SpringRunner
@@ -16,7 +21,6 @@ import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
-import sun.plugin2.util.PojoUtil.toJson
 import java.util.*
 
 @RunWith(SpringRunner::class)
@@ -37,15 +41,8 @@ class ApiV1ControllerUnitTests {
     @MockBean
     private lateinit var repository: JpaRepository<Any, Any>
 
-    @Test
-    fun find() {
-        given(repository.findAll()).willReturn(listOf())
-        given(service.forEntityName("person")).willReturn(repository)
-
-        mvc.perform(get("$baseUrl/people"))
-                .andExpect(status().isOk)
-                .andExpect(content().json("[]"))
-    }
+    @Autowired
+    private lateinit var objectMapper: ObjectMapper
 
     @Test
     fun get() {
@@ -59,24 +56,42 @@ class ApiV1ControllerUnitTests {
     }
 
     @Test
-    fun postAndPut() {
-        val person = Person()
-        given(repository.save(person)).willReturn(person)
-        given(service.forEntityName("person")).willReturn(repository)
+    fun find() {
+        val content = listOf(given())
+        val page = PageImpl(content) as Page<Any>
+        given(repository.findAll(ArgumentMatchers.any(Pageable::class.java))).willReturn(page)
 
-        mvc.perform(post("$baseUrl/people").contentType(MediaType.APPLICATION_JSON).content(toJson(person)))
+        val json = objectMapper.writeValueAsString(page)
+        mvc.perform(get("$baseUrl/people"))
                 .andExpect(status().isOk)
-                .andExpect(content().json("{}"))
-
-        mvc.perform(put("$baseUrl/people").contentType(MediaType.APPLICATION_JSON).content(toJson(person)))
-                .andExpect(status().isOk)
-                .andExpect(content().json("{}"))
+                .andExpect(content().json(json))
     }
 
+    @Test
+    fun postAndPut() {
+        val person = given()
+        val json = objectMapper.writeValueAsString(person)
+
+        mvc.perform(post("$baseUrl/people").contentType(MediaType.APPLICATION_JSON).content(json))
+                .andExpect(status().isOk)
+                .andExpect(content().json(json))
+
+        mvc.perform(put("$baseUrl/people").contentType(MediaType.APPLICATION_JSON).content(json))
+                .andExpect(status().isOk)
+                .andExpect(content().json(json))
+    }
 
     @Test
     fun asEntitySimpleName() {
         assertThat(impl.asEntitySimpleName("people")).isEqualTo("person")
         assertThat(impl.asEntitySimpleName("documents")).isEqualTo("document")
+    }
+
+    private fun given(): Person {
+        val person = Person()
+        given(repository.save(person)).willReturn(person)
+        given(service.forEntityName("person")).willReturn(repository)
+        given(service.getEntityClass("person")).willReturn(Person::class.java)
+        return person
     }
 }
